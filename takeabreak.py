293fb34d40bd68d3832d61bf166d3bbfc5d12367
Analin Flores <foglzerika@gmail.com>
2017-09-17 07:06:15 +0200

    Adding a loop

--- takeabreak.py
+++ takeabreak.py
@@ -1,4 +1,10 @@
 import webbrowser
 import time
-time.sleep(5)
-webbrowser.open("https://www.youtube.com/watch?v=oNWOC_Pvo4w")
+total_breaks = 3
+count = 0
+while (count < total_breaks) :
+    count +=1
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=oNWOC_Pvo4w")
+    print 'Time to break'
+    print count
